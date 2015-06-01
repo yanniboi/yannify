9bd2dc87aae09d845fadf365b4a09bd015a76390
yanniboi <yan@yanniboi.com>
2015-06-01 14:40:04 +0100

    Added blank constants file.

---
+++ www/js/constants.js
@@ -0,0 +1,6 @@
+var module = angular.module('spotify.constants',[]);
+
+module.constant('client_id', '');
+module.constant('client_secret', '');
+module.constant('spotify_user', '');
+module.constant('spotify_playlist', '');
